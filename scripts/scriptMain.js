10bf16565bdebee3b8619bd56faccb29d3faf3ce
peduu87 <pepe.silva1202@gmail.com>
2024-08-25 22:22:56 -0300

    closes #9 - travel cards

--- scripts/scriptMain.js
+++ scripts/scriptMain.js
@@ -35,3 +35,8 @@ function waitForElement(selector, callback) {
 
 waitForElement("#navBar", navLinkActive);
 waitForElement("#navBar", dropdownItemActive);
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
